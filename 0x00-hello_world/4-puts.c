cb41904cd33c8298979b6ead95f3433e010f95dd
obedwayne <obedwayne17@gmail.com>
2022-12-12 15:32:30 -0800

    ronaldo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Use put to print out a statement
+*Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
