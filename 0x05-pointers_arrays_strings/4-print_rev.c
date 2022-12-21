596a41875886ce235110bff1d7993b8f9ea243f2
obedwayne <obedwayne17@gmail.com>
2022-12-20 19:42:55 -0800

    l

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,11 +3,12 @@
 /**
  *print_rev - prints a string, in reverse, followed by a new line.
  *
- *
+ *@s: a string to be printed.
  */
 void print_rev(char *s)
 {
 	int index;
+
 	for (index = 0; s[index] != '\0'; index++)
 		;
 	for (index = index - 1; s[index] != '\0'; index--)
