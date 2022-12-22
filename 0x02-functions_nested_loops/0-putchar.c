c4870ad85a83669132ba7b96527020ea9c030e40
obedwayne <obedwayne17@gmail.com>
2022-12-22 14:12:15 -0800

    push

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters
+ * @c ; The character to be checked.
+ *
+ * Returns; 1 if character is uppercase. 0 if otherwise.
+ */
+
+int _isupper(int c)
+{
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef' MAIN.H
+#define MAIN.H
+
+/*
+ * file-main.h
+ * Author: Wayne
+ * Desc: Header contains prototype for all functions
+ * 	used in 0x02-functions_nested_loops
+ */
+
+int _putchar(char c);
+
+
+
+
+#endif
